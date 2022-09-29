697f29911bf5713ba8ea9bce6e22957f0392d52d
Niloy-Dey <niloydeyce@gmail.com>
2022-09-29 10:40:48 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Light-on-off-project-tutorial-usign-html-css-js" 
---
+++ index.js
@@ -0,0 +1,7 @@
+function lightOn(){
+    document.getElementById('light').src='light-on.png';
+}
+
+function lightOff(){
+    document.getElementById('light').src='light-off.png';
+}
